b68d27f6afefd51912c58d2fb7e87ebeffcfa8e9
John Yunker <jyunker@bytelevel.com>
2020-11-20 17:33:53 -0800

    changed

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -8,7 +8,7 @@ function CurrentlySlicing({ slicemasters }) {
   return (
     <div>
       <h2 className="center">
-        <span className="mark tilt">Slicemasters On</span>
+        <span className="mark tilt">Slicemasters Working Now!</span>
       </h2>
       <p>Standing by, ready to slice you up!</p>
       {!slicemasters && <LoadingGrid count={4} />}
